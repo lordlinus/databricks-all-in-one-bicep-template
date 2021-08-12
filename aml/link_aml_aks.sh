0fdb323205803ef3675470dbc525fc262a6e74b1
Sunil Sattiraju <sunilsattiraju@gmail.com>
2021-08-12 08:46:01 +0800

    Update link_aml_aks.sh
    capture az cli output

--- aml/link_aml_aks.sh
+++ aml/link_aml_aks.sh
@@ -1,2 +1,4 @@
+#/bin/bash -e
 az extension add -n azure-cli-ml -y
-az ml computetarget attach aks -n myaks -i "$AKS_ID" -g "$RG" -w "$WORKSPACE_NAME"
+json=$(az ml computetarget attach aks -n myaks -i "$AKS_ID" -g "$RG" -w "$WORKSPACE_NAME")
+echo "$json" >$AZ_SCRIPTS_OUTPUT_PATH
